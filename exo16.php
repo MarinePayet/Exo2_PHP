0cc53fbf418715ee340171c7105a1f0942e52935
Marine PAYET <marine.payet3@gmail.com>
2023-05-09 14:41:50 +0200

    nb premier

---
+++ exo16.php
@@ -0,0 +1,25 @@
+<?php
+
+function isPrime($number) {
+
+    for ($i = 2; $i < $number-1; $i++) {
+        if ($number % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+$count = 0;
+
+$number = 2;
+
+while($count<5) {
+    if(isPrime($number)){
+        $count++;
+        echo $number . "<br>";
+        $number++;
+    } else {
+        $number++;
+    }
+}
