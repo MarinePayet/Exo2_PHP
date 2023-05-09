57b3dd1bc07adf640fe9c124be76bc618c9f5148
Marine PAYET <marine.payet3@gmail.com>
2023-05-09 10:05:55 +0200

    afficher 5 films

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
 echo "Hello World !";
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th> Amélie Poulain </th>
+            <th> Love Actually </th>
+            <th> The Holiday </th>
+            <th> 6ème sens </th>
+            <th> Fight Club </th>
+        </thead>
+    </tr>
+</table>
