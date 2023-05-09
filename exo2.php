d56aaf6fbed4309fd8d63d6d6e05dbb67c003899
Marine PAYET <marine.payet3@gmail.com>
2023-05-09 10:16:42 +0200

    new fichier

---
+++ exo2.php
@@ -0,0 +1,15 @@
+<?php
+
+$movies = [ "Amélie Poulain", "Love Actually", "The Holiday", "6ème Sens", "Fight Club"];
+
+foreach($movies as $movie){
+    
+    ?> 
+    <ul>
+        <li><?php echo $movie ?></li>
+    </ul>
+    
+
+<?php };
+
+
--- index.php
+++ index.php
@@ -2,18 +2,3 @@
 
 echo "Hello World !";
 
-
-$movies = [ "Amélie Poulain", "Love Actually", "The Holiday", "6ème Sens", "Fight Club"];
-
-foreach($movies as $movie){
-    
-    ?> 
-    <ul>
-        <li><?php echo $movie ?></li>
-    </ul>
-    
-
-<?php };
-
-
-
